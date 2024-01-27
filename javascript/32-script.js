7bac77bfd730ac105745963f4e2203474e37fb77
Kamal Barman <117052862+kamalbarman@users.noreply.github.com>
2024-01-27 15:06:37 +0530

    Update and rename 32-script.html to 32-script.js

---
+++ javascript/32-script.js
@@ -0,0 +1,8 @@
+<!-- write this html code given below -->
+<h3>display in alertbox</h3>
+
+<!-- now write the js code -->
+   
+        let arr = [1, 2, 3, 4, 5,7,8,9];
+        let length = arr.length;
+        alert("The length of the array is: " + length);
