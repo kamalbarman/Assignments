1306ecb4affcc99dafbc24b6e2b2f149e6ccc2ca
Kamal Barman <kamalbarman1528@gmail.com>
2024-06-19 11:37:15 +0530

    edited the c++ question number 1

--- CppAssignments/1-triangleSide.cpp
+++ CppAssignments/1-triangleSide.cpp
@@ -2,61 +2,34 @@
 
 using namespace std;
 
-class Triangle
-{
-private:
-    int side1;
-    int side2;
-    int side3;
+class Triangle{
 
-public:
-    Triangle(int s1 = 0, int s2 = 0, int s3 = 0)
-    {
-        side1 = s1;
-        side2 = s2;
-        side3 = s3;
-    }
-    void getdata()
-    {
-        cout << "enter the  length of three sides :->" << endl;
+    public:
+         int side1=0;
+         int side2=0;
+         int side3=0;
+    
+    void getdata(){
+        cout <<"enter the side parameters";
         cin >> side1 >> side2 >> side3;
     }
 
-    bool isosceles()
-    {
-        return (side1 == side2 || side1 == side3 || side2 == side3);
+    void calcu(){
+        if (side1 == side2 && side2 == side3)
+        {
+           cout << "It is an equilateral triangle." << endl;
+        }else if (side1 == side2 || side1 == side3 || side2 == side3){
+             cout << "It is an isosceles triangle." << endl;
+        }else{
+            cout << "it is a scalene triangle" << endl;
+        }
+        
     }
-
-    bool equilateral()
-    {
-        return (side1 == side2 && side1 == side3 && side2 == side3);
-    }
+   
 };
-
-int main()
-{
-
+int main(){
     Triangle t1;
 
     t1.getdata();
-
-    if (t1.isosceles())
-    {
-        cout << "the triangle is isosceles." << endl;
-    }
-    else
-    {
-        cout << "the triangle is not isosceles." << endl;
-    }
-
-    if (t1.equilateral())
-    {
-        cout << "the triangle is equilateral." << endl;
-    }
-    else
-    {
-        cout << "the triangle is not equilateral." << endl;
-    }
-
-    return 0;
+    t1.calcu();
 }
