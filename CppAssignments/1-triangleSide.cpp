de9cf42fd68f703233f9cb5d3d8b401e7475c32f
Kamal Barman <kamalbarman1528@gmail.com>
2024-06-20 16:36:04 +0530

    improved the question  1-triangle code

--- CppAssignments/1-triangleSide.cpp
+++ CppAssignments/1-triangleSide.cpp
@@ -2,34 +2,39 @@
 
 using namespace std;
 
-class Triangle{
+class Triangle
+{
 
-    public:
-         int side1=0;
-         int side2=0;
-         int side3=0;
-    
-    void getdata(){
-        cout <<"enter the side parameters";
+public:
+    int side1 = 0;
+    int side2 = 0;
+    int side3 = 0;
+
+    void getdata()
+    {
+        cout << "enter the side parameters";
         cin >> side1 >> side2 >> side3;
     }
 
-    void calcu(){
-        if (side1 == side2 && side2 == side3)
-        {
-           cout << "It is an equilateral triangle." << endl;
-        }else if (side1 == side2 || side1 == side3 || side2 == side3){
-             cout << "It is an isosceles triangle." << endl;
-        }else{
-            cout << "it is a scalene triangle" << endl;
-        }
-        
+    bool Isosceles() {
+        return (side1 == side2) || (side1 == side3) || (side2 == side3);
     }
-   
+
+    bool Equilateral() {
+        return (side1 == side2) && (side2 == side3);
+    }
 };
-int main(){
+int main()
+{
     Triangle t1;
 
     t1.getdata();
-    t1.calcu();
+
+    if (t1.Equilateral()) {
+        cout << "Equilateral" << endl;
+    } else if (t1.Isosceles()) {
+        cout << "Isosceles" << endl;
+    } 
+  
+    return 0;
 }
